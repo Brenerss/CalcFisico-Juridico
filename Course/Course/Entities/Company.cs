538a1f12b66e98f0520c98c4412bd4f4aa84c2b8
Brener Sousa <brener.dev@gmail.com>
2020-01-18 01:24:25 -0300

    Metods Abstratos e Polimorfismo C#

---
+++ Course/Course/Entities/Company.cs
@@ -0,0 +1,25 @@
+﻿namespace Course.Entities
+{
+    class Company : TaxPayer
+    {
+        public int Employers { get; set; }
+
+        public Company() { }
+
+        public Company(string name, double anualIncome, int employers) : base(name, anualIncome)
+        {
+            Employers = employers;
+        }
+
+        public override double Tax()
+        {
+            if (Employers > 10)
+            {
+                return AnualIncome * 0.14;
+            } else
+            {
+                return AnualIncome * 0.16;
+            }
+        }
+    }
+}
---
+++ Course/Course/Entities/Individual.cs
@@ -0,0 +1,26 @@
+﻿namespace Course.Entities
+{
+    class Individual : TaxPayer
+    {
+        public double HealthExpenditures { get; set; }
+
+
+        public Individual() { }
+
+        public Individual(string name, double anualIncome, double healthExpeditures) : base(name, anualIncome)
+        {
+            HealthExpenditures = healthExpeditures;
+        }
+
+        public override double Tax()
+        {
+            if(AnualIncome < 20000)
+            {
+                return AnualIncome * 0.15 - HealthExpenditures * 0.5;
+            } else
+            {
+                return AnualIncome * 0.25 - HealthExpenditures * 0.5;
+            }
+        }
+    }
+}
